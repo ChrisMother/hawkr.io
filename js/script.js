8add41208e858b3bc54f54ad13b8e06a266bd0f5
gaffhawkr <chris@hawkr.io>
2016-03-09 12:02:39 +0000

    New DOOH site

--- js/script.js
+++ js/script.js
@@ -24,7 +24,29 @@ jQuery(document).ready(function(){
       jQuery(".navbar-nav a").click(function(){
             $.scrollTo(document.getElementById(jQuery(this).attr("data-href")), 1500 );
       });
-      
+
+      $("#feature-slider-2").owlCarousel({navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      pagination : false,
+      navigation: false,
+      autoHeight : true,
+      afterMove: function(data){
+            jQuery(".feature-section").removeClass(box_item );
+            box_item_index = this.currentItem;
+            box_item = box_items[box_item_index ];
+            jQuery(".feature-section").addClass(box_item );
+
+            jQuery(".feature-category .category-item").removeClass("active");
+            jQuery(".feature-category .category-item:nth-child(" + parseInt(box_item_index + 1 ) + ")").addClass("active");
+
+      }});
+      owl2 = $("#feature-slider-2").data('owlCarousel');
+      jQuery(".navbar-nav a").click(function(){
+            $.scrollTo(document.getElementById(jQuery(this).attr("data-href")), 1500 );
+      });
+
        jQuery(".contactus-item input, .contactus-item textarea").keypress(function(){
             jQuery(this).parent().find(".contact-form-alert").css("display", "none" );
        });
@@ -36,17 +58,17 @@ jQuery(document).ready(function(){
                   return;
             }
             if (jQuery("#contact-address").val() == "" ){
-                  jQuery("#contact-address").parent().find(".contact-form-alert").css("display", "block" );      
+                  jQuery("#contact-address").parent().find(".contact-form-alert").css("display", "block" );
                   return;
             }
 
             if (jQuery("#contact-mobile").val() == "" ){
-                  jQuery("#contact-mobile").parent().find(".contact-form-alert").css("display", "block" );      
+                  jQuery("#contact-mobile").parent().find(".contact-form-alert").css("display", "block" );
                   return;
             }
 
             if (jQuery("#contact-message").val() == "" ){
-                  jQuery("#contact-message").parent().find(".contact-form-alert").css("display", "block" );      
+                  jQuery("#contact-message").parent().find(".contact-form-alert").css("display", "block" );
                   return;
             }
 
@@ -71,7 +93,7 @@ jQuery(document).ready(function(){
             jQuery(".contactus-form").css("display", "block" );
             jQuery(".modal-back").animate({opacity: 1}, 500, function(){});
             jQuery(".contactus-form").animate({opacity: 1}, 500, function(){});
-      });      
+      });
 
       jQuery(".close-btn").click(function(){
             jQuery(".modal-back").animate({opacity: 0}, 500, function(){
@@ -91,18 +113,21 @@ jQuery(document).ready(function(){
                   jQuery(".contactus-form").animate({opacity: 0}, 500, function(){
                         jQuery(".contactus-form").css("display", "none" );
                   });
-            }            
+            }
       });
 });
 
 function gotoFeature(ind ){
 	owl.goTo(ind );
+      owl2.goTo(ind );
 }
 
 function nextFeature(){
 	owl.next();
+      owl2.next();
 }
 
 function prevFeature(){
 	owl.prev();
+      owl2.prev();
 }
